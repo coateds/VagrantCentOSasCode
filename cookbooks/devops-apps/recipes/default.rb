7a812b23cd4b20c4aadd2869aa35fd002de0e54d
Dave Coate <coateds@outlook.com>
2019-02-28 10:56:59 -0800

    refactor devops apps to install WANDisco git-all

--- cookbooks/devops-apps/recipes/default.rb
+++ cookbooks/devops-apps/recipes/default.rb
@@ -1,4 +1,4 @@
-package "git"
+# package "git"
 
 remote_file '/etc/pki/rpm-gpg/microsoft.asc' do
     source 'https://packages.microsoft.com/keys/microsoft.asc'
@@ -18,3 +18,20 @@ end
 package "code" do
     flush_cache [ :before ]
 end
+
+# Add/Configure the wandisco repo to get the latest version of Git
+remote_file '/etc/pki/rpm-gpg/RPM-GPG-KEY-WANdisco' do
+    source 'http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco'
+    action :create
+end
+
+file "/etc/yum.repos.d/wandisco-git.repo" do
+    content "[WANdisco-git]
+name=WANdisco Distribution of git
+baseurl=http://opensource.wandisco.com/rhel/$releasever/git/$basearch
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-WANdisco"
+end
+
+package "git-all"
