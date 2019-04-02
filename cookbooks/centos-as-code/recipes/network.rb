9591ba79fdf6f8d076c12245530736adf6672d1b
coateds <coateds@outlook.com>
2019-04-02 08:47:18 -0700

    Sequim Notes

---
+++ cookbooks/centos-as-code/recipes/network.rb
@@ -0,0 +1,8 @@
+# Look at recipes here:
+# https://github.com/coateds/windows-installation-recipes/tree/master/recipes
+# for some good code examples
+
+# Add entries to /etc/hosts
+# Add aliases to .bashrc file
+
+# write conidtional to test/build string for log output
